fdf88a0ff27b33d4bb711bcd766b407f7ed0cb2f
nathlinda2024 <163464380+nathlinda2024@users.noreply.github.com>
2024-03-14 17:42:56 -0300

    Add files via upload
    Alterações efetuadas na aula do dia 14 de março de 2024

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>
+
+<!-- container que abriga o rodapé da página -->
+<div class="container-fluid text-center bg-dark text-light fixed-bottom">
+   <P>Sistema web desenvolvido por Nath</P>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suplements TB </title>
+
+    <!-- última versão compilada CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- última versão compilada JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+    <div class="container-fluid text-center text-dark">
+        <img src="img/logo.webp" width= "100">
+        <h1 style="font-family:Kode Mono">Suplements TB</h1>
+    </div>
+
+ <!--- barra de navegação do sistema --->
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php" title="Ir para a Página Inicial">Página inicial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Cadastrar Suplementos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+ <!-- container que abriga o conteúdo da página -->
+ <div class="container text-center mt-5 mb-5">
